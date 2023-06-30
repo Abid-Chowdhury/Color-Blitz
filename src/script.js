ed951c533173bb037f8791db9700e3549990503c
Abid Chowdhury <58383484+Abid1732@users.noreply.github.com>
2023-06-30 19:44:40 -0400

    Color input value updates Added color value

--- src/script.js
+++ src/script.js
@@ -1,4 +1,22 @@
-for (i = 0; i < 100; i++) {
-    const inp = document.getElementById('input')
-    conso
-}
+// UPDATE COLOR INPUT VALUE
+const redSlider = document.getElementById('redInput')
+const redValueDisplay = document.getElementById('red-guess')
+const greenSlider = document.getElementById('greenInput')
+const greenValueDisplay = document.getElementById('green-guess')
+const blueSlider = document.getElementById('blueInput')
+const blueValueDisplay = document.getElementById('blue-guess')
+
+redSlider.addEventListener("input", () => {
+    const value = redSlider.value
+    redValueDisplay.innerHTML = value
+})
+
+greenSlider.addEventListener("input", () => {
+    const value = greenSlider.value
+    greenValueDisplay.innerHTML = value
+})
+
+blueSlider.addEventListener("input", () => {
+    const value = blueSlider.value
+    blueValueDisplay.innerHTML = value
+})
