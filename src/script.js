692d98ee9147f72cad6ae5307cf5df342e8bea2b
Abid Chowdhury <58383484+Abid1732@users.noreply.github.com>
2023-06-30 21:38:25 -0400

    Updated colors container

--- src/script.js
+++ src/script.js
@@ -6,20 +6,37 @@ const greenValueDisplay = document.getElementById('green-guess')
 const blueSlider = document.getElementById('blueInput')
 const blueValueDisplay = document.getElementById('blue-guess')
 
+var redGuess = 0
+var greenGuess = 0
+var blueGuess = 0
+
 redSlider.addEventListener("input", () => {
     const value = redSlider.value
+    redGuess = value
+    updateGuessColor()
     redValueDisplay.innerHTML = value
 })
 
 greenSlider.addEventListener("input", () => {
     const value = greenSlider.value
+    greenGuess = value
+    updateGuessColor()
     greenValueDisplay.innerHTML = value
 })
 
 blueSlider.addEventListener("input", () => {
     const value = blueSlider.value
+    blueGuess = value
+    updateGuessColor()
     blueValueDisplay.innerHTML = value
 })
+
+// UPDATED GUESS COLOR CONTAINER
+function updateGuessColor() {
+    const guess_RGB = `rgb(${redGuess}, ${greenGuess}, ${blueGuess})`
+    const guessColorContainer = document.getElementById('guess-color-container')
+    guessColorContainer.style.backgroundColor = guess_RGB
+}
 
 // SET RANDOM COLOR
 function setRandomColor() {
